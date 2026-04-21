e161733f0a1dc9f5a0cad5b9a1dec0ea6c5c581a
pratyushpandeyy <88322550+pratyushpandeyy@users.noreply.github.com>
2026-04-21 16:36:12 +0530

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        int currEnd = 0;
+        int jumps = 0;
+        int maxReach = 0;
+
+        if(nums.size() == 1) return 0;
+
+        for(int i = 0; i < nums.size() - 2; i++)
+        {
+            maxReach = max(maxReach, nums[i] + currEnd);
+
+            if (i == currEnd)
+            {
+                jumps++;
+                currEnd = maxReach;
+            }
+        }
+
+        return jumps;
+        
+    }
+};
